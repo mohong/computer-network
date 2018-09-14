93bc2ea8aeaf7113f8d341e134b1ac55d3b13202
mohong <arthas_qin@qq.com>
2018-09-14 14:03:00 +0800

    增加侧边栏导航配置

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,5 +1,31 @@
 module.exports = {
   base: '/computer-network/',
   title: '我的笔记',
-  description: 'Just playing around'
+  description: 'Just playing around',
+  themeConfig: {
+    sidebar: [
+      ['/', '概述'],
+      {
+        title: '物理层',
+        collapsable: false,
+        children: []
+      },
+      {
+        title: '数据链路层',
+        children: [ /* ... */]
+      },
+      {
+        title: '网络层',
+        children: [ /* ... */]
+      },
+      {
+        title: '运输层',
+        children: [ /* ... */]
+      },
+      {
+        title: '应用层',
+        children: [ /* ... */]
+      }
+    ]
+  }
 }
