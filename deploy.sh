b0e6faa141b7b67fe8c4368a4762f1ae927af66f
mohong <arthas_qin@qq.com>
2018-09-14 11:10:08 +0800

    完善发布脚本

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,19 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-git add -A
-git commit -m 'deploy'
-
 cd ..
 # 生成静态文件
 vuepress build computer-network
 
 cd -
 
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:mohong/computer-network.git master:gh-pages
 
+cd -
