e042bd26fba16e61180ffd3ecf509288c96cef05
mohong <arthas_qin@qq.com>
2018-09-14 11:02:53 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -7,11 +7,11 @@ git add -A
 git commit -m 'deploy'
 
 cd ..
-
 # 生成静态文件
 vuepress build computer-network
+
+cd -
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:mohong/computer-network.git master:gh-pages
 
-cd -
